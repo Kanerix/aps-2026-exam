74865b197b003ea85dbb8ed708bfcf4522a707ca
AkselDyhr <akdy@itu.dk>
2026-05-04 11:21:47 +0200

    cookie_selection implemented, not tested yet

--- cookie_selection.py
+++ cookie_selection.py
@@ -1,1 +1,32 @@
 print("Cookie Selection")
+
+import heapq
+import sys
+
+
+lowMaxHeap = []
+highMinHeap = []
+
+for line in sys.stdin:
+    line = line.strip()
+    if line == '#':
+        popValue = heapq.heappop(highMinHeap)
+        print(popValue)
+
+        #rebalance
+        if len(highMinHeap) < len(lowMaxHeap):
+            heapq.heappush(highMinHeap, -heapq.heappop(lowMaxHeap))
+    else:
+        n = int(line)
+        if highMinHeap and n >= highMinHeap[0]:
+            heapq.heappush(highMinHeap, n)
+        else:
+            heapq.heappush(lowMaxHeap, -n) #negative to reverse maxheap to minheap
+
+
+        #rebalance
+        if len(highMinHeap) > len(lowMaxHeap) + 1:
+            heapq.heappush(lowMaxHeap, -heapq.heappop(highMinHeap))
+        elif len(lowMaxHeap) > len(highMinHeap):
+            heapq.heappush(highMinHeap, -heapq.heappop(lowMaxHeap))
+
