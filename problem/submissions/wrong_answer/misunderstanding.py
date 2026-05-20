f3b2532844827b5bfaf3f820f8f62f8775a7fb6c
Matthias Nyman <mail@natthias.dev>
2026-05-20 02:22:39 +0200

    add misunderstanding wrong solution

---
+++ problem/submissions/wrong_answer/misunderstanding.py
@@ -0,0 +1,59 @@
+def bfs(s, t, parent, graph):
+    visited = [False] * len(graph)
+    queue = []
+    queue.append((s, float("inf")))
+    visited[s] = True
+
+    while queue:
+        u, flow = queue.pop(0)
+        for v, cap in graph[u].items():
+            if not visited[v] and cap > 0:
+                parent[v] = u
+                visited[v] = True
+                max_flow = min(flow, cap)
+                if v == t:
+                    return max_flow
+
+                queue.append((v, max_flow))
+
+    return 0
+
+
+def max_flow(s, t, graph):
+    parent = [-1] * len(graph)
+    max_flow = 0
+
+    while True:
+        path_flow = bfs(s, t, parent, graph)
+        if path_flow == 0:
+            break
+
+        max_flow += path_flow
+
+        v = t
+        while v != s:
+            u = parent[v]
+            if u not in graph[v]:
+                graph[v][u] = 0
+
+            graph[u][v] -= path_flow
+            graph[v][u] += path_flow
+            v = u
+
+    return max_flow
+
+
+def main():
+    p, m, s, t, m_s, m_t = input().split()
+    graph: list[dict[int, int]] = [{} for _ in range(int(m))]
+    for _ in range(int(p)):
+        u, v, c = input().split()
+        if u == m_s and v == m_t:
+            continue
+
+        graph[int(u)][int(v)] = int(c)
+
+    print(max_flow(int(s), int(t), graph))
+
+
+main()
